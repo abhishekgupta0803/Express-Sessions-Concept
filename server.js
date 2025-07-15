4ddacd4b13aee88f473b7dd575002c5853e76925
AbhishekGupta <=>
2025-07-15 14:17:02 +0530

    Add

--- server.js
+++ server.js
@@ -1,26 +1,38 @@
-//Create a route Sent a request Number of times
+//example of how to use flash messages
 const express = require("express");
 const app = express();
-const session = require('express-session');
+const session = require("express-session");
+const flash = require("connect-flash"); //flash messages
+const path = require("path");
 
-app.use(session({secret:"mysecretecode",resave:false,saveUninitialized:true}));
+//storing and using session information
+const sessionsOptions = {
+  secret: "mysecretecode",
+  resave: false,
+  saveUninitialized: true,
+};
 
-app.get("/test",(req,res)=>{
-    res.send("test successful");
-})
+//views set middleware
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+//sessions middleware
+app.use(session(sessionsOptions));
+app.use(flash());
 
-app.get("/reqcount",(req,res)=>{
-    if(req.session.count){
-        req.session.count++;
-    }else{
-         req.session.count = 1;
-    }
+app.get("/register", (req, res) => {
+  let { name = "anonymous" } = req.query;
+  req.session.name = name;
+  req.flash("success", "user registered successfully"); //this is a key , message pair store
+  res.redirect("/hello");
+});
 
-    res.send(`you sent a request${ req.session.count++} number of times`);
-})
+app.get("/hello", (req, res) => {
+  res.render("flash.ejs", {
+    name: req.session.name,
+    msg: req.flash("success"),
+  }); //success is a flash message key
+});
 
-
-
-app.listen(3000,()=>{
-    console.log("App is listening on port");
+app.listen(3000, () => {
+  console.log("App is listening on port");
 });
